b45f9f2079d6f5ee2286bb143d6f2447e54ac580
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-16 16:07:28 -0800

    Janise Adams CIS261 Guessing Game

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,1 +1,35 @@
+import random
 
+def title():
+    print("Guess the number!")
+    print()
+    print()
+
+def playing(LIMIT):
+    number = random.randint(1, LIMIT)
+    print(f"Im thinking of a number from 1 to {LIMIT}\n.")
+    count = 1
+    guess = int(input("Your guess: "))
+
+    while (guess != number):
+        if guess < number:
+            print("Too low.")
+            count += 1
+        elif guess > number:
+            print("Too high.")
+            count += 1
+        guess = int(input("Your guess: "))
+    print(f"You guessed it in {count} tries.\n")
+
+def main():
+    title()
+    again = "y"
+    while again.lower() == "y":
+        LIMIT = int(input("Enter the limit: "))
+        playing(LIMIT)
+        again = input("Would you like to play again? (y/n): ")
+        print()
+    print("Bye!")
+
+if __name__ == "__main__":
+    main()
